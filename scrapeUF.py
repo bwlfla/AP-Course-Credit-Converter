4b69ffdc5c2192116eeda86664578f858f015728
brian.lu <brian.lu@ufl.edu>
2020-04-18 23:47:42 -0400

    Found the table of AP exams

--- scrapeUF.py
+++ scrapeUF.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 import openpyxl
 
 workbook=openpyxl.Workbook()
@@ -8,5 +9,11 @@ my_sheet.title= "UF"
 filePath="C:\\Users\\Brian\\OneDrive\\Documents\\Computer Science\\MAT4930\\Final Project\\AP-Course-Credit-Converter\\"
 nameOfFile="UF.xlsx"
 
+url="https://catalog.ufl.edu/UGRD/academic-advising/exam-credit/#examstext"
+page=requests.get(url)
+soup=BeautifulSoup(page.content,'html.parser')
+
+results=soup.find_all('table',summary='AP Exam')
+print(results)
+
 workbook.save(filePath+nameOfFile)
-print("Hello World")
