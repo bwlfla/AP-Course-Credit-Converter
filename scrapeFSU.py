5f49cd8a2a631485a8e4fb434d561947eade2f23
brian.lu <brian.lu@ufl.edu>
2020-04-21 15:16:56 -0400

    Added comments and references for FSU webscraping

--- scrapeFSU.py
+++ scrapeFSU.py
@@ -16,20 +16,22 @@ my_sheet['D1']="Score of 5"
 
 
 url="https://admissions.fsu.edu/credit/AP/"
+#performs an HTTP request to the given URL
 page=requests.get(url)
+#the contents of page are formatted by BeautifulSoup for easy usage/access
 soup=BeautifulSoup(page.content,'html.parser')
-
-#tableOfInfo=soup.find('tbody')
+#rows creates an array/arraylist of all the instances of the tag 'tr' with the field bgcolor='#ffffff'
 rows=soup.find_all('tr',bgcolor='#ffffff')
-#print(rows)
 
 counter=2
 for row in rows:
+	#creates an array/arraylist of all the instances of the tag 'td' that can be accessed by index
 	division=row.findAll('td')
+
 	apExamName=division[0].text.strip()
 	credit3=division[1].text.strip()
 	credit4=division[2].text.strip()
-
+	#there is one element in the html of the fsu website that does not appear after the webscraping occurs
 	try:
 		credit5=division[3].text.strip()
 	except IndexError:
@@ -40,15 +42,17 @@ for row in rows:
 	if("Same as" in credit5):
 		credit5=credit4
 
-
+	#place the strings in the right cells in excel
 	my_sheet['A'+(str(counter))]=apExamName
 	my_sheet['B'+(str(counter))]=credit3
 	my_sheet['C'+(str(counter))]=credit4
 	my_sheet['D'+(str(counter))]=credit5
 	counter=counter+1
 
+#saves the excel file at the filePath
 workbook.save(filePath+nameOfFile)
 
-#might not do since fsu physics c and magnetism has a tag of ==$0 on the side that makes it disappear
-
-#IndexError is thrown maybe do a trycatch
+'''Sources used:
+https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
+https://realpython.com/beautiful-soup-web-scraper-python/
+'''
