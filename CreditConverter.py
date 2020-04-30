44c1121139af98e618b485cf527c6382e1cc769e
gracewilcox <graceawilcox@gmail.com>
2020-04-30 12:52:35 -0400

    finished version

--- CreditConverter.py
+++ CreditConverter.py
@@ -1,41 +1,26 @@
-#CHARLES COURSE, PRINT OUT FORM
-
 import pandas as pd
-#CANT INPUT SAME EXAM TWICE
-#num to FSU equivalent
-#num to UF equivalent
-#ap menu
-#make main functions and side functions
-#object oriented??
 
-#object of all??
-#course equivalent object containing fsu and uf
-
-class Course:
+class Course: # course class stores the ap exam and its course equivalency at UF and FSU
 	def __init__(self, examNum, score):
-		self.examNum = examNum
-		self.score = score
-		#self.examName = ""
-		self.UFClass = ""
-		self.UFCredit = 0
-		self.FSUClass = ""
-		self.FSUCredit = 0
-		self.findUF()
-		self.findFSU()
-		#findexamname
+		self.examNum = examNum # menu number of AP exam
+		self.score = score # AP exam score
+		self.examName = APdict[examNum] # name of AP exam
+		self.UFClass = "" # name of equivalent class at UF
+		self.UFCredit = 0 # number of credits earned at UF
+		self.FSUClass = "" # name of equivalent class at FSU
+		self.FSUCredit = 0 # name of equivalent class at FSU
+		self.findUF() # finds class name and credits at UF
+		self.findFSU() # finds class name and credits at FSU
 	
-	#def findClassName(self):
-	
-	#first name, then credits!
-	
-	def findUF(self):
-		name = UFdf.iat[self.examNum,(self.score - 2)]
+	def findUF(self): # finds UF equivalency information
+		name = UFdf.iat[self.examNum,(self.score - 2)] # retrieves course information from excel
 		
-		if "(" in name: #if parenthesis, then number of credits is specified uniquely 
+		if "(" in name: #if parenthesis, then number of credits is specified uniquely (need to extract credit info from name)
 			x = name.split("(")
 			self.UFClass = x[0]
 			self.UFCredit = int((x[1])[0])
-		else:
+			
+		else: # no specific credit information, can follow general format
 			self.UFClass = name
 			if (self.score == 3):
 				self.UFCredit = 3
@@ -43,138 +28,126 @@ class Course:
 				self.UFCredit = 6
 	
 	def findFSU(self):
-		num = self.examNum
-		FSUnum = 0
-		
-		#conversion to FSU order
-		if (1 <= num <= 2):
-			FSUnum = num + 42
-		elif (3 <= num <= 6):
-			FSUnum = num - 3
-		elif (num == 7):
-			FSUnum = 6
-		elif (num == 8):
-			FSUnum = 5
-		elif (9 <= num <= 11):
-			FSUnum = num - 2
-		elif (num == 12):
-			FSUnum = 11
-		elif (num == 13):
-			FSUnum = 42
-		elif (14 <= num <= 18):
-			FSUnum = num - 2
+	
+		# need dictionary to convert between menu numbering and numbering in FSU excel spreadsheet
+		FSUdict = {
+			1: 43, 2: 44, 3: 0, 4: 1, 5: 2,
+			6: 3, 7: 6, 8: 5, 9: 7, 10: 8, 11: 9,
+			12: 11, 13: 42, 14: 12, 15: 13, 16: 14,
+			17: 15, 18: 16, 19: 22, 20: 17, 21: 19,
+			22: 20, 23: 21, 24: 25, 25: 26, 26: 27, 27: 29,
+			28: 30, 29: 32, 30: 33, 31: 34, 32: 35,
+			33: 36, 34: 37, 35: 39, 36: 40,37: 41,
+			38: 23, 39: 24
+		}
 			
-		print(FSUdf.iat[FSUnum,self.score - 2])
-		
-		#finding the scores
-		
+		name = FSUdf.iat[FSUdict[self.examNum], self.score - 2] # retrieves FSU info from spreadsheet
+		x = name.split(")")
+		x.pop()
+		for i in x : # extracts name and number of credits
+			self.FSUClass += i[:-2]
+			self.FSUCredit = self.FSUCredit + int(i[-1:])
 	
 
-#possible library class, contains all courses and statistics like total number of credits at each school, display function??
-
-def menu1():
-	print("\nAP Exam Menu")
+def menu1(): #menu where new exams are added
+	
+	print("\nAP Exam Menu") # menu displays all ap exams
 	print("------------")
-	print("1. 2-D Art and Design")
-	print("2. 3-D Art and Design")
-	print("3. Art History")
-	print("4. Biology")
-	print("5. Calculus AB")
-	print("6. Calculus BC")
-	print("7. Capstone Research")
-	print("8. Capstone Seminar")
-	print("9. Chemistry")
-	print("10. Chinese Language and Culture")
-	print("11. Computer Science A")
-	print("12. Computer Science Principles")
-	print("13. Drawing")
-	print("14. Economics: Macro")
-	print("15. Economics: Micro")
-	print("16. English Language and Composition")
-	print("17. English Literature and Composition")
-	print("18. Environmental Science")
-	print("19. European History")
-	print("20. French Language")
-	print("21. German Language")
-	print("22. Govt. and Politics: Comparative")
-	print("23. Govt. and Politics: United States")
-	print("24. Human Geography")
-	print("25. Italian Language and Culture")
-	print("26. Japanese Language and Culture")
-	print("27. Latin")
-	print("28. Music Theory")
-	print("29. Physics 1")
-	print("30. Physics 2")
-	print("31. Physics B")
-	print("32. Physics C: Electricity and Magnetism")
-	print("33. Physics C: Mechanics")
-	print("34. Psychology")
-	print("35. Spanish Language")
-	print("36. Spanish Literature")
-	print("37. Statistics")
-	print("38. United States History")
-	print("39. World History: Modern")
-	examNum = int(input("Please select an AP exam: "))
+	for x in APdict:
+		print(str(x) + ". " + APdict[x])
+
+	examNum = int(input("Please select an AP exam: ")) # user selects desired ap exam
 
 	print("\n")
-	score = int(input("Please enter score (1-5) earned on exam: "))
+	score = int(input("Please enter score (1-5) earned on exam: ")) # user enters exam score
 	
-	if (score == 1 or score == 2):
+	if (score == 1 or score == 2): # no credit is earned for scoring a 1 or a 2
 		print("Score too low, no credit earned.") 
-	elif( score < 1 or score > 5):
+	elif( score < 1 or score > 5): # scores must be between 1 and 5
 		print("invalid score")
-	else:
+	else: # if valid score, add to list of earned courses
 		c1 = Course(examNum, score)
 		courses.append(c1)
+		print("AP " + courses[-1].examName + " is equivalent to " + courses[-1].UFClass + "(" + str(courses[-1].UFCredit) + " credits) at UF and " + courses[-1].FSUClass + "(" + str(courses[-1].FSUCredit) + " credits) at FSU")
 
-courses = [];
+# dictionary of all ap exam names
+APdict = {
+		1 : "2-D Art and Design",
+		2: "3-D Art and Design",
+		3: "Art History",
+		4: "Biology",
+		5: "Calculus AB",
+		6: "Calculus BC",
+		7: "Capstone Research",
+		8: "Capstone Seminar",
+		9: "Chemistry",
+		10: "Chinese Language and Culture",
+		11: "Computer Science A",
+		12: "Computer Science Principles",
+		13: "Drawing",
+		14: "Economics: Macro",
+		15: "Economics: Micro",
+		16: "English Language and Composition",
+		17: "English Literature and Composition",
+		18: "Environmental Science",
+		19: "European History",
+		20: "French Language",
+		21: "German Language",
+		22: "Govt. and Politics: Comparative",
+		23: "Govt. and Politics: United States",
+		24: "Human Geography",
+		25: "Italian Language and Culture",
+		26: "Japanese Language and Culture",
+		27: "Latin",
+		28: "Music Theory",
+		29: "Physics 1",
+		30: "Physics 2",
+		31: "Physics B",
+		32: "Physics C: Electricity and Magnetism",
+		33: "Physics C: Mechanics",
+		34: "Psychology",
+		35: "Spanish Language",
+		36: "Spanish Literature",
+		37: "Statistics",
+		38: "United States History",
+		39: "World History: Modern"
+	}
+	
+courses = []; # initialize array of course objects
 exit = False
 
+#loading course equivalency information from excel spreadsheets into a pandas dataframe
 UFdf = pd.read_excel("UF.xlsx")
-FSUdf = pd.read_excel("Computational MathFSU.xlsx")
+FSUdf = pd.read_excel("FSU.xlsx")
 
 print("\nWelcome to the AP credit Converter!")
 print("This program converts AP scores into the course equivalents at the University of Florida and Florida State University.")
 
-while(exit == False):
+while(exit == False): # loops main command window until user decides to exit
 	
 	print("\nCredit Converter Main Menu")
 	print("1. Add Exam")
 	print("2. View All Exams")
 	print("3. Exit")
-	menuOption = input("Please select an option: ")
+	menuOption = input("Please select an option: ") # user selects menu option
 	
 	
-	if (menuOption == "1"):
-		menu1()
-	elif(menuOption == "2"):
-		for x in courses:
-			print(x.score)
-	elif (menuOption == "3"):
+	if (menuOption == "1"): # option 1 is add an ap exam
+		menu1() # branches to menu that gathers ap information and adds new course object to list
+		
+	elif(menuOption == "2"): # displays all current equivalency information
+		totUFCredits = 0
+		totFSUCredits = 0
+		for x in courses: #prints information for each ap equivalency
+			print("AP " + x.examName + " is equivalent to " + x.UFClass + "(" + str(x.UFCredit) + " credits) at UF and " + x.FSUClass + "(" + str(x.FSUCredit) + " credits) at FSU")
+			totUFCredits = totUFCredits + x.UFCredit
+			totFSUCredits = totFSUCredits + x.FSUCredit
+		print("\nTotal credits earned at UF: " + str(totUFCredits)) # displays total credits earned at UF from ap exams
+		print("Total credits earned at FSU: " + str(totFSUCredits)) # displays total credits earned at FSU from ap exams
+		
+	elif (menuOption == "3"): # exits the program
 		print("Thank you for using Credit Converter. Goodbye!")
 		exit = True
+		
 	else:
 		print("invalid input")
-	
-	
-	
-
-
-# TRY CATCH FOR INVALID INPUTS
-#display exam grades
-#display cummulative exam scores
-
-
-
-
-
-
-
-#menu selecting AP exam
-
-#menu selecting AP exam grade
-
-#exit and more exam grade options
-
-
-#display running total
